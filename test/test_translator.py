a7612d873d04ef21753a20462a4e85a6bcd5bbda
johcue <lianagar914@gmail.com>
2025-10-30 16:03:06 +0100

    Red phase for US7 punctuation marks

--- test/test_translator.py
+++ test/test_translator.py
@@ -77,3 +77,10 @@ class TestPigLatinTranslator(TestCase):
         translator = PigLatinTranslator("hello well-being")
         translation = translator.translate()
         self.assertEqual("ellohay ellway-eingbay", translation)
+
+    def test_translate_phrase_with_punctuation_marks(self):
+        translator = PigLatinTranslator("hello world!")
+        translation = translator.translate()
+        self.assertEqual("ellohay orldway!", translation)
+
+
